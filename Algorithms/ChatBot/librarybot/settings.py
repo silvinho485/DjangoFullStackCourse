6aef7d6c8222d7601f1ae25934b742b48018d3fb
Silvinho <silvinho485@gmail.com>
2018-01-30 10:32:36 -0200

    Update settings.py

--- Algorithms/ChatBot/librarybot/settings.py
+++ Algorithms/ChatBot/librarybot/settings.py
@@ -6,7 +6,7 @@ PLATFORMS = {
     'telegram': {
         'ENGINE': 'bottery.platform.telegram',
         'OPTIONS': {
-            'token': '472876997:AAGNuejBRK_XEr8s6lRYsRd9mQkOu4kcLUM',
+            'token': '',
         }
     },
 }
